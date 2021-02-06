5c475ff77e647f6389984c1c2fc0432367b84c05
Mirshod <mirshod.mirjonov@mail.ru>
2021-02-06 14:13:25 +0500

    imports

---
+++ src/javaSE1/a/b/PackageTest.java
@@ -0,0 +1,8 @@
+package javaSE1.a.b;
+/* Mirshod created on 2/6/2021 */
+
+public class PackageTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
